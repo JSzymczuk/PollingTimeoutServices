8423b0d8de80604a5fcbf8929fab77271616748d
jakub.szymczuk@plte.pl <jakub.szymczuk@plte.pl>
2018-11-06 18:37:29 +0100

    Szkielet projektu.

---
+++ TimeoutService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TimeoutService.Startup))]
+namespace TimeoutService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
