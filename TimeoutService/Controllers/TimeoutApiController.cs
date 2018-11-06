32e40c46bdc5762c842722d0db7ce5db728169fe
jakub.szymczuk@plte.pl <jakub.szymczuk@plte.pl>
2018-11-06 19:26:04 +0100

    Dodana możliwość wywołania usługi.

---
+++ TimeoutService/Controllers/TimeoutApiController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace TimeoutService.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class TimeoutApiController : ApiController
+    {
+        [HttpGet]
+        [Route("Timeout/")]
+        public HttpResponseMessage Get()
+        {
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("Get, mordy!", Encoding.UTF8, "application/json")
+            };
+        }
+
+        [HttpPost]
+        [Route("Timeout/")]
+        public HttpResponseMessage Post()
+        {
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("Post mordy!", Encoding.UTF8, "application/json")
+            };
+        }
+
+        [HttpPut]
+        [Route("Timeout/")]
+        public HttpResponseMessage Put()
+        {
+            return new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent("Zażółć gęślą jaźń, mordy!", Encoding.UTF8, "application/json")
+            };
+        }
+    }
+}
